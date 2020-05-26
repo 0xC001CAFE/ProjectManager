66366351e6798033f5cb18691f0be7c2a58d374c
Robin <r.leinauer@gmx.de>
2020-05-26 16:41:35 +0200

    Added standard data access services

---
+++ ProjectManager.Domain/Services/IDataService.cs
@@ -0,0 +1,19 @@
+﻿using ProjectManager.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManager.Domain.Services
+{
+    public interface IDataService<T> where T : DomainBase
+    {
+        Task<T> GetById(string id);
+
+        Task<T> Create(T entity);
+
+        Task<T> UpdateById(string id, T entity);
+
+        Task<bool> DeleteById(string id);
+    }
+}
---
+++ ProjectManager.Domain/Services/IUserAccountService.cs
@@ -0,0 +1,19 @@
+﻿using ProjectManager.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManager.Domain.Services
+{
+    public interface IUserAccountService : IDataService<UserAccount>
+    {
+        Task<UserAccount> GetByUsername(string username);
+
+        Task<UserAccount> GetByEmail(string email);
+
+        Task<bool> UsernameExists(string username);
+
+        Task<bool> EmailExists(string email);
+    }
+}
