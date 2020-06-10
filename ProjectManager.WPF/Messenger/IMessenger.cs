cb6dcac1f2ae43c34b46a40a56a7b756bca8c63d
Robin <r.leinauer@gmx.de>
2020-06-10 21:13:41 +0200

    Added a messenger to communicate across view models

---
+++ ProjectManager.WPF/Messenger/IMessenger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectManager.WPF.Messenger
+{
+    public interface IMessenger
+    {
+        void Register<T>(Action<T> callback);
+
+        void Unregister<T>(object receiver);
+
+        void Send<T>(T message);
+    }
+}
