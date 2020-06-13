f9a9f37563c6b3fc7f0f07529dabe6a5c2efa3c9
Robin <r.leinauer@gmx.de>
2020-06-13 22:31:29 +0200

    File formatting corrected

--- ProjectManager.WPF/Messaging/Messages/PropertyChangedMessage.cs
+++ ProjectManager.WPF/Messaging/Messages/PropertyChangedMessage.cs
@@ -4,13 +4,13 @@ using System.Text;
 
 namespace ProjectManager.WPF.Messaging.Messages
 {
-	public class PropertyChangedMessage<T>
-	{
-		public T PropertyValue { get; private set; }
+    public class PropertyChangedMessage<T>
+    {
+        public T PropertyValue { get; private set; }
 
-		public PropertyChangedMessage(T propertyValue)
-		{
-			PropertyValue = propertyValue;
-		}
-	}
+        public PropertyChangedMessage(T propertyValue)
+        {
+            PropertyValue = propertyValue;
+        }
+    }
 }
