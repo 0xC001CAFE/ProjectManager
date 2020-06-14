da28e46893aa3b163306299e02966fb344bcdcd9
Robin <r.leinauer@gmx.de>
2020-06-14 21:08:02 +0200

    Added a view for editing a project

---
+++ ProjectManager.WPF/ViewModels/EditableProjectViewModel.cs
@@ -0,0 +1,32 @@
+﻿using ProjectManager.Domain.Models;
+using ProjectManager.WPF.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectManager.WPF.ViewModels
+{
+    public class EditableProjectViewModel : ViewModelBase
+    {
+        #region Properties for data binding
+
+        private Project editableProject;
+        public Project EditableProject
+        {
+            get => editableProject;
+            private set
+            {
+                editableProject = value;
+
+                OnPropertyChanged(nameof(EditableProject));
+            }
+        }
+
+        #endregion
+
+        public EditableProjectViewModel(IMessenger messenger) : base(messenger)
+        {
+
+        }
+    }
+}
