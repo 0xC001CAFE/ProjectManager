be39ba7f90618780eb3af826b004a5d9b4e7f39f
Robin <r.leinauer@gmx.de>
2020-06-15 21:58:25 +0200

    Added a simple implementation of a command

---
+++ ProjectManager.WPF/Commands/RelayCommand.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace ProjectManager.WPF.Commands
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action execute;
+        private readonly Func<bool> canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                if (canExecute != null) CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                if (canExecute != null) CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        public RelayCommand(Action execute, Func<bool> canExecute = null)
+        {
+            this.execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute == null || canExecute();
+        }
+
+        public void Execute(object parameter)
+        {
+            execute();
+        }
+    }
+
+    public class RelayCommand<T> : ICommand
+    {
+        private readonly Action<T> execute;
+        private readonly Predicate<T> canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                if (canExecute != null) CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                if (canExecute != null) CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        public RelayCommand(Action<T> execute, Predicate<T> canExecute = null)
+        {
+            this.execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return canExecute == null || canExecute((T)parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            execute((T)parameter);
+        }
+    }
+}
